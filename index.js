dc68bc7b50008f0a2974d22f84b19dd4356178cc
sachinacro <sachinsaini08883@gmail.com>
2025-07-07 13:28:36 +0530

    file

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ import adminRoutes from "./routes/admin.js";
 import router from "./routes/quizRoute.js";
 
 // ✅ Using routes
-app.use("/api", userRoutes);
-app.use("/api", courseRoutes);
-app.use("/api", adminRoutes);
-app.use("/api", router);
+app.use("/api/api", userRoutes);
+app.use("/api/api", courseRoutes);
+app.use("/api/api", adminRoutes);
+app.use("/api/api", router);
 
 // ✅ Start server
 app.listen(port, () => {
